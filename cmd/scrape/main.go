d5c385cd0f55b67df88425a694c4f2cf776c7ebe
Torben Dury <94752317+torbendury@users.noreply.github.com>
2023-09-11 19:33:16 +0200

    feat: basic input validation

--- cmd/scrape/main.go
+++ cmd/scrape/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/torbendury/scrape-go/pkg/scrape"
 )
@@ -15,6 +16,11 @@ func main() {
 	allowUrlDuplicates := flag.Bool("allow-duplicate-urls", false, "Allow duplicated links. Only takes effect if URL scraping is active.")
 	scrapeImages := flag.Bool("images", false, "NOT IMPLEMENTED YET: Set to true if you want to scrape images.")
 	flag.Parse()
+
+	if *baseUrl == "" || *linkDepth < 1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	scraper := scrape.NewScraper(*baseUrl, *scrapeUrls, *urlFile, *linkDepth, *allowUrlDuplicates, *scrapeImages)
 
